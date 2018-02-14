7d13c61f9b67b791aabd024fdb5493fbd55afc12
ila <milos.ilic@bitgear.rs>
2018-02-14 19:44:15 +0100

    unpack array

---
+++ src/Core/UnpackMapFromArray.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ila
+ * Date: 14.2.18.
+ * Time: 19.32
+ */
+
+namespace Bgw\Core;
+
+
+class UnpackMapFromArray implements IUnpack
+{
+
+    // __DIR__ . "/comm-app-config.ini"
+
+    /* @var string
+     * */
+    private $path;
+
+    function getUnpackMap(): array
+    {
+        return require_once ($this->path);
+    }
+
+    function setPathToFile(string $path)
+    {
+        $this->path = $path;
+    }
+}
